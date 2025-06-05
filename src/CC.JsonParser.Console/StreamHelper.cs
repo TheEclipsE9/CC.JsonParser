e5b9641c58e686b560c77171696a6e897a30b38d
TheEclipsE <TheEclipsE@tut.by>
2025-06-05 18:26:55 +0200

    clear the whole shit

--- src/CC.JsonParser.Console/StreamHelper.cs
+++
@@ -1,9 +0,0 @@
-using System.Text;
-
-namespace CC.JsonParser.Console;
-
-public static class StreamHelper
-{
-    public static Stream ToStream(this string str)
-        => new MemoryStream(Encoding.UTF8.GetBytes((str)));
-}
