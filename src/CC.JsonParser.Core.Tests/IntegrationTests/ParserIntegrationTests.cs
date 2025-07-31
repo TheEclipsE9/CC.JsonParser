c896fc7fb2ab73017f0ca22faaac0030f7fcc13b
TheEclipsE <TheEclipsE@tut.by>
2025-07-31 22:27:21 +0200

    add test fro invalid cases should throw exceptions

--- src/CC.JsonParser.Core.Tests/IntegrationTests/ParserIntegrationTests.cs
+++ src/CC.JsonParser.Core.Tests/IntegrationTests/ParserIntegrationTests.cs
@@ -6,15 +6,10 @@ public class ParserIntegrationTests
 {
     [Theory]
     [InlineData("tests/step1/valid.json", true)]
-//    [InlineData("tests/step1/invalid.json", false)]
     [InlineData("tests/step2/valid.json", true)]
     [InlineData("tests/step2/valid2.json", true)]
-//    [InlineData("tests/step2/invalid.json", false)]
-//    [InlineData("tests/step2/invalid2.json", false)]
     [InlineData("tests/step3/valid.json", true)]
-//    [InlineData("tests/step3/invalid.json", false)]
     [InlineData("tests/step4/valid.json", true)]
-//    [InlineData("tests/step4/invalid.json", false)]
     [InlineData("tests/step4/valid2.json", true)]
     public void ParserIntegration_FromJsonFile_NoException(string path, bool shouldSucceed)
     {
@@ -29,5 +24,31 @@ public class ParserIntegrationTests
 
         var parser = new Parser();
         var jsonObject = parser.Parse(tokens);
+    }
+
+    [Theory]
+    [InlineData("tests/step1/invalid.json", false)]
+    [InlineData("tests/step2/invalid.json", false)]
+    [InlineData("tests/step2/invalid2.json", false)]
+    [InlineData("tests/step3/invalid.json", false)]
+    [InlineData("tests/step4/invalid.json", false)]
+    public void ParserIntegration_FromJsonFile_WithException(string path, bool shouldSucceed)
+    {
+        var exeLocation = AppContext.BaseDirectory;
+        var src = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+        var pathToFile = Path.Combine(src, path);
+
+        var input = File.ReadAllText(pathToFile);
+
+        var sutAction = () =>
+        {
+            var lexer = new Lexer();
+            var tokens = lexer.Tokenize(input);
+
+            var parser = new Parser();
+            var jsonObject = parser.Parse(tokens);
+        };
+
+        Assert.ThrowsAny<Exception>(sutAction);
     }
 }
