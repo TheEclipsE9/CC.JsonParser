77aabe50b321683864f9c68f156a5d41916997fe
TheEclipsE <TheEclipsE@tut.by>
2025-07-31 22:17:48 +0200

    add tests against files

---
+++ src/CC.JsonParser.Core.Tests/IntegrationTests/ParserIntegrationTests.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace CC.JsonParser.Core.Tests.IntegrationTests;
+
+public class ParserIntegrationTests
+{
+    [Theory]
+    [InlineData("tests/step1/valid.json", true)]
+//    [InlineData("tests/step1/invalid.json", false)]
+    [InlineData("tests/step2/valid.json", true)]
+    [InlineData("tests/step2/valid2.json", true)]
+//    [InlineData("tests/step2/invalid.json", false)]
+//    [InlineData("tests/step2/invalid2.json", false)]
+    [InlineData("tests/step3/valid.json", true)]
+//    [InlineData("tests/step3/invalid.json", false)]
+    [InlineData("tests/step4/valid.json", true)]
+//    [InlineData("tests/step4/invalid.json", false)]
+    [InlineData("tests/step4/valid2.json", true)]
+    public void ParserIntegration_FromJsonFile_NoException(string path, bool shouldSucceed)
+    {
+        var exeLocation = AppContext.BaseDirectory;
+        var src = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+        var pathToFile = Path.Combine(src, path);
+
+        var input = File.ReadAllText(pathToFile);
+
+        var lexer = new Lexer();
+        var tokens = lexer.Tokenize(input);
+
+        var parser = new Parser();
+        var jsonObject = parser.Parse(tokens);
+    }
+}
